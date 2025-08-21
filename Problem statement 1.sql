3b53b387d9f1b63a973f80d3389abca55e889743
Ronisaha2003 <162054100+Ronisaha2003@users.noreply.github.com>
2025-08-21 21:32:41 +0530

    Add files via upload

---
+++ Problem statement 1.sql
@@ -0,0 +1,10 @@
+-- Retrieve the total number of orders placed. question no 1
+-- Answer of first question
+
+
+-- project mysql problem statements:
+-- Retrieve the total number of orders placed.
+-- Calculate the total revenue generated from pizza sales.
+-- Identify the highest-priced pizza.
+-- Identify the most common pizza size ordered.
+select count(order_id) from orders;
